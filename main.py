c77c9aecd2133ddbe8a5f78af50ee6bf1caeb18f
vikybenky <63884562+vikybenky@users.noreply.github.com>
2023-12-09 14:36:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,7 @@ st.title('Landmark Recognition')
 uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
-
-    if st.button('Say hello'):
-        st.write('Why hello there')
+    st.image(uploaded_file)
+    
+    if st.button('Run'):
+        st.write('AI is running...')
