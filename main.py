991c7fb99a4a278bcef861eec513a24f4404466b
vikybenky <63884562+vikybenky@users.noreply.github.com>
2023-12-11 19:47:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,31 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from io import StringIO
+import joblib
 
 st.title('Landmark Recognition')
 
-uploaded_file = st.file_uploader("Choose an image to be uploaded:", type="jpg")
+uploaded_file = st.file_uploader("Choose an image to classify:", type="jpg")
 
 if uploaded_file is not None:
     st.image(uploaded_file)
     
     if st.button('Run'):
-        st.write('AI is running...')
-
-        # model = load_model('my_model.h5')
         
-        # prediction = model.predict(test_X, verbose=1) 
+        with st.spinner('AI is running...'):
+            cat_clf=joblib.load("model.pkl")
+
+            image = np.asarray(u_img)/255
+            my_image= resize(image, (64,64)).reshape((1, 64*64*3)).T
+        
+            model = load_model('model.pkl')
+            
+            prediction = model.predict(test_X, verbose=1)
+            
+            time.sleep(2)
+            
+            st.success('Done!')
+
+        
+    
+
